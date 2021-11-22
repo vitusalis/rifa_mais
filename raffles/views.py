4b68442711f4f50bf7593f2c5f5aca0c4ef5c9ca
vitusalis <victorsalles1997@gmail.com>
2021-11-22 20:07:39 -0300

    Make field mandatory and restrict API

--- raffles/views.py
+++ raffles/views.py
@@ -15,24 +15,25 @@ class AuthenticatedOrPostOnly(permissions.BasePermission):
     message = "Only authenticated users may put or delete"
 
     def has_permission(self, request, view):
-        if request.method in permissions.SAFE_METHODS or request.method == 'POST':
+        if request.method in permissions.SAFE_METHODS or request.method == "POST":
             return True
         return False
 
 
 class CsrfExemptSessionAuthentication(SessionAuthentication):
-
     def enforce_csrf(self, request):
         return  # To not perform the csrf check previously happening
 
 
-@api_view(['GET'])
+@api_view(["GET"])
 def api_root(request, format=None):
-    return Response({
-        'users': reverse('user-list', request=request, format=format),
-        'raffles': reverse('raffle-list', request=request, format=format),
-        'tickets': reverse('ticket-list', request=request, format=format)
-    })
+    return Response(
+        {
+            "users": reverse("user-list", request=request, format=format),
+            "raffles": reverse("raffle-list", request=request, format=format),
+            "tickets": reverse("ticket-list", request=request, format=format),
+        }
+    )
 
 
 class UserViewSet(viewsets.ReadOnlyModelViewSet):
@@ -40,7 +41,7 @@ class UserViewSet(viewsets.ReadOnlyModelViewSet):
     serializer_class = UserSerializer
 
 
-class RaffleViewSet(viewsets.ModelViewSet):
+class RaffleViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Raffle.objects.all()
     serializer_class = RaffleSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
@@ -53,7 +54,7 @@ class TicketViewSet(viewsets.ModelViewSet):
     authentication_classes = (CsrfExemptSessionAuthentication, BasicAuthentication)
 
     def get_queryset(self):
-        raffle_id = self.request.query_params.get('raffle', None)
+        raffle_id = self.request.query_params.get("raffle", None)
         if raffle_id:
             return Ticket.objects.filter(raffle_id=raffle_id).order_by("ticket_number")
         return Ticket.objects.all()
@@ -61,3 +62,12 @@ class TicketViewSet(viewsets.ModelViewSet):
     def list(self, request, *args, **kwargs):
         result = TicketSerializer(self.get_queryset(), many=True, context={"request": request})
         return Response(result.data)
+
+    def update(self, request, *args, **kwargs):
+        return Response({}, status=403)
+
+    def partial_update(self, request, *args, **kwargs):
+        return Response({}, status=403)
+
+    def destroy(self, request, *args, **kwargs):
+        return Response({}, status=403)
